67dfb1a9fc1e1752a7c85881ca9a059a03b73e21
Yuan Fang <jessie_yuan@yeah.net>
2015-01-19 13:53:20 +0800

    问卷开放题文本分析，提取关键词

---
+++ analysis.py
@@ -0,0 +1,81 @@
+import jieba, jieba.analyse
+
+def text_split(text):
+    return text.upper().strip().split('\n')
+
+def extract_keywords(text, method = "tfidf", num = 10, pos = ['ns', 'n', 'vn', 'v']):
+    if method == 'tfidf':
+        kw = jieba.analyse.extract_tags(text, num, withWeight = True, allowPOS=pos)
+    elif method == 'textrank':
+        kw = jieba.analyse.textrank(text, num, withWeight = True, allowPOS=pos)
+    return {w[0]: w[1] for w in kw}
+
+def seg_text(text_list, HMM=True):
+    import jieba
+    seg = []
+    for t in text_list:
+        try:
+            seg.append(" ".join(jieba.cut(t, HMM=HMM)).split())
+        except:
+            seg.append(" ")
+    seg_list = [[w for w in s] for s in seg]
+    return seg_list
+
+def filter_words(keywords, text):
+    text_list = text_split(text)
+    seg_list = seg_text(text_list)
+    kw_list = keywords.keys()
+    return [[w for w in seg if w in kw_list] for seg in seg_list]
+
+def words_count(word, word_list, text):
+    words_count = {w: list(sum(word_list, [])).count(w) for w in word}
+    for key in words_count.keys():
+        if words_count[key] == 0:
+            words_count[key] = text.count(key)
+    return words_count
+
+def keywords_count(keywords, filter_list, text):
+    kw_list = keywords.keys()
+    kw_count = words_count(kw_list, filter_list, text)
+    filter_list_unique = [list(set(li)) for li in filter_list]
+    kw_user = words_count(kw_list, filter_list_unique, text)
+    return kw_count, kw_user
+
+def merge_keywords(kw_weight, kw_count, kw_user):
+    keywords = {}
+    for key, value in kw_weight.items():
+        keywords.setdefault(key, {})['weight'] = value
+        keywords.setdefault(key, {})['count'] = kw_count.get(key, 0)
+        keywords.setdefault(key, {})['user_num'] = kw_user.get(key, 0)
+    return keywords
+
+def generate_network(words_list):
+    src = []
+    dst = []
+    for s in words_list:
+        i = 0
+        s2 = list(set(s))
+        s2.sort()
+        while (i < len(s2)):
+            j = i + 1
+            while (j < len(s2)):
+                src.append(s2[i])
+                dst.append(s2[j])
+                j += 1
+            i += 1
+    vertice = list(set(src))
+    return vertice, src, dst
+
+def text_processing(text, method='tfidf', num=10, pos=['ns', 'n', 'vn', 'v']):
+    kw = extract_keywords(text, method='tfidf', num=text.count('\n'), pos=pos)
+    filter_list = filter_words(kw, text)
+    kw_count, kw_user = keywords_count(kw, filter_list, text)
+    keywords = merge_keywords(kw, kw_count, kw_user)
+    print " ".join(keywords.keys())
+    return keywords
+
+
+
+
+
+
---
+++ app.py
@@ -0,0 +1,76 @@
+# coding: utf-8
+
+import sys
+import os
+from flask import Flask, render_template, session, redirect, url_for
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import TextAreaField, SubmitField, IntegerField, SelectField, BooleanField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms.validators import Required, Optional
+from werkzeug import secure_filename
+import analysis
+import json
+import chardet
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+
+app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+class TextForm(Form):
+    text = TextAreaField('请输入需要提取关键词的文本')
+    upload = FileField('文件上传', validators=[
+        Optional(), FileAllowed(['txt'], '只能上传txt格式的文件！')
+        ])
+    num = IntegerField('关键词个数', validators=[Required()])
+    method = SelectField('提取方法', choices=[('tfidf', 'tfidf'), ('textrank', 'textrank')])
+    #pos_n = BooleanField('名词')
+    #pos_v = BooleanField('动词')
+    #pos_a = BooleanField('名词')
+    #pos_ad = BooleanField('副词')
+    submit = SubmitField('提交')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def inter_server_error(e):
+    return render_template('500.html'), 500
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    text = None
+    filedata = None
+    form = TextForm()
+    keywords = {}
+    if form.validate_on_submit():
+        if form.upload.data:
+            filedata = form.upload.data
+            text = "".join([line.decode(chardet.detect(line)['encoding']) for line in filedata.readlines()])
+            session['text'] = text
+            print secure_filename(form.upload.data.filename)
+            #form.upload.data.save()
+        else:
+            session['text'] = form.text.data
+        session['num'] = form.num.data
+        session['method'] = form.method.data
+        #num = session['text'].count('\n') + 1
+        keywords = analysis.text_processing(session.get('text'), method=session['method'], num=session['num'], pos=['n'])
+        session['keywords'] = "'" + json.dumps(keywords, ensure_ascii=False) + "'"
+        return redirect(url_for('result', resultstr=session.get('keywords')))
+    return render_template('index.html', form=form)
+
+@app.route('/result/<resultstr>', methods=['GET', 'POST'])
+def result(resultstr):
+    return render_template('result.html', keywords=resultstr)
+
+if __name__ == '__main__':
+    manager.run()
