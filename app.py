3e11391dd8e30ce3a51f69735b1d9e2aa47a573b
Yuan Fang <jessie_yuan@yeah.net>
2015-01-20 16:18:51 +0800

    增加选择关键词词性功能

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ from flask import Flask, render_template, session, redirect, url_for, request
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
-from wtforms import TextAreaField, SubmitField, IntegerField, SelectField, BooleanField
-from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms.validators import Required, Optional
+from wtforms import TextAreaField, SubmitField, IntegerField, SelectField, SelectMultipleField
+from flask_wtf.file import FileField
 from werkzeug import secure_filename
 import analysis
 import json
@@ -32,15 +31,18 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 class TextForm(Form):
     text = TextAreaField('请输入需要提取关键词的文本')
-    upload = FileField('文件上传', validators=[
-        Optional(), FileAllowed(['txt'], '只能上传txt格式的文件！')
-        ])
-    num = IntegerField('关键词个数', validators=[Required()])
-    method = SelectField('提取方法', choices=[('tfidf', 'tfidf'), ('textrank', 'textrank')])
-    #pos_n = BooleanField('名词')
-    #pos_v = BooleanField('动词')
-    #pos_a = BooleanField('名词')
-    #pos_ad = BooleanField('副词')
+    upload = FileField('文件上传')
+    num = IntegerField('关键词个数')
+    method = SelectField('提取方法', choices=[('tfidf', 'tf-idf'), ('textrank', 'textrank')])
+    pos = SelectMultipleField('选择关键词的词性（可用ctrl多选）',
+        choices=[('pos_n', '名词'), ('pos_v', '动词'), ('pos_a', '形容词'), ('pos_d', '副词'), ('pos_z', '专名'), ('pos_o', '其他（成语、习用语等）')]
+        )
+    #pos_n = BooleanField('名词', false_values=False)
+    #pos_v = BooleanField('动词', false_values=False)
+    #pos_a = BooleanField('形容词', false_values=False)
+    #pos_d = BooleanField('副词', false_values=False)
+    #pos_z = BooleanField('专名', false_values=False)
+    #pos_o = BooleanField('其他（成语、习用语等）', false_values=False)
     submit = SubmitField('提交')
 
 def get_salt(chars = string.letters + string.digits):
@@ -78,16 +80,22 @@ def result():
             if allowed_file(uploadfile.filename):
                 text = "".join([line.decode(chardet.detect(line)['encoding']) for line in uploadfile.readlines()])
             else:
-                return rediect(url_for('index'))
+                return redirect(url_for('index'))
         else:
             text = request.form['text']
             if not text:
-                return rediect(url_for('index'))
+                return redirect(url_for('index'))
         num = int(request.form['num'])
         method = request.form['method']
-        keywords, network, vertice = analysis.text_processing(text, method=method, num=num, pos=['n'])
+        pos_code = {'pos_a': ['Ag', 'a', 'ad', 'an'], 'pos_d': ['Dg', 'd'], 'pos_n': ['n', 'Ng'], 'pos_o': ['l', 'i'], 'pos_v': ['v', 'vn'], 'pos_z': ['nr', 'ns', 'nt', 'nz']}
+        pos = []
+        for p, l in pos_code.items():
+            if request.form['pos'] == p:
+                pos = pos + l
+        if pos == []:
+            pos = ['ns', 'n', 'vn', 'v']
+        keywords, network, vertice = analysis.text_processing(text, method=method, num=num, pos=pos)
         result = "'" + json.dumps(keywords, ensure_ascii=False) + "'"
-        print result
     return render_template('result.html', keywords=result, vertice=vertice, network=network)
 
 if __name__ == '__main__':
